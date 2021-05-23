c5a348c95144cb262d188f41ff0f3e3fa8483e8a
Eduardo Barreto Alexandre <sezdocs@gmail.com>
2021-05-23 12:15:23 -0300

    Bumps version to 1.1.0-rc.6

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule RZstd.MixProject do
   @app :r_zstd
   @name "RZstd"
   @description "Rust powered NIF bindings to Facebook's Zstd library"
-  @version "1.1.0-rc.5"
+  @version "1.1.0-rc.6"
   @github "https://github.com/sezaru/#{@app}"
   @author "Eduardo Barreto Alexandre"
   @license "MIT"
